895026f8bcd29a6ba00428b71c0374ca4ed255cd
miltonzini <miltonzini@gmail.com>
2023-09-03 21:19:38 -0300

    Update FirstComponent.jsx
    test variables

--- src/FirstComponent.jsx
+++ src/FirstComponent.jsx
@@ -1,6 +1,24 @@
+const exampleString = 'text'
+const exampleNumber = 123
+const exampleArray = ['text', 123, 5, 'text' ]
+const exampleBoolean = true
+const exampleFunction = () => 1+1*10
+const exampleObject = { day: 'monday', number: 5}
+const exampleDate = new Date()
+
 export const FirstComponent = () => {
   return (
-    <h1>Hello World!</h1>
+    <>
+        <h1>Variables!</h1>
+
+        <p>{ exampleString }</p>
+        <p>{ exampleNumber }</p>
+        <p>{ exampleArray }</p>
+        <p>{ exampleBoolean }</p>
+        <p>{ exampleFunction() }</p>
+        <p>{ JSON.stringify(exampleObject) }</p>
+        <p>{ JSON.stringify(exampleDate) }</p>
+    </>
   )
 }
 
