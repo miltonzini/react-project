5ba808170a52d6b0b8296c89ccd572dc41d5c9ba
miltonzini <miltonzini@gmail.com>
2023-09-14 18:19:18 -0300

    add global styles and component syles

--- src/FirstComponent.jsx
+++ src/FirstComponent.jsx
@@ -1,3 +1,5 @@
+import './FirstComponent.css'
+
 const exampleString = 'text'
 const exampleNumber = 123
 const exampleArray = ['text', 123, 5, 'text' ]
@@ -6,17 +8,30 @@ const exampleFunction = () => 1+1*10
 const exampleObject = { day: 'monday', number: 5}
 const exampleDate = new Date()
 
+
 export const FirstComponent = () => {
   return (
     <>
         <h1>Variables!</h1>
-
+        <h4>String type variable</h4>
         <p>{ exampleString }</p>
+
+        <h4>Number type variable</h4>
         <p>{ exampleNumber }</p>
+
+        <h4>Array type variable</h4>
         <p>{ exampleArray }</p>
+
+        <h4>Boolean type variable</h4>
         <p>{ exampleBoolean }</p>
+
+        <h4>String type variable</h4>
         <p>{ exampleFunction() }</p>
+        
+        <h4>String type variable</h4>
         <p>{ JSON.stringify(exampleObject) }</p>
+        
+        <h4>String type variable</h4>
         <p>{ JSON.stringify(exampleDate) }</p>
     </>
   )
